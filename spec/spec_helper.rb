f0ef12cd35f8c8d966d9946d7a39ed7021eb9cc1
Léo <leo.hetsch@gmail.com>
2017-07-06 18:26:07 +0100

    Add tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,23 @@
 require "bundler/setup"
 require "sightengine"
+require 'webmock/rspec'
+
+class MockClient < Sightengine::Client
+  include WebMock::API
+
+  WebMock.enable!
+
+  def mock(model, url, expected)
+    stub_request(:get, build_uri('check.json')).
+      with(query: {
+        url: url,
+        api_user: api_user,
+        api_secret: api_secret,
+        models: model
+      }).
+      to_return(body: expected.to_json)
+  end
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
